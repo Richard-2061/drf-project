029badad6b3b397f816371db281d1078d71d65db
Richard-2061 <gavinstar2006@gmail.com>
2025-03-19 12:04:41 +0800

    Delete drf-project directory

--- drf-project/users/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Richard
-# @File: apps.py
-from django.apps import AppConfig
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
